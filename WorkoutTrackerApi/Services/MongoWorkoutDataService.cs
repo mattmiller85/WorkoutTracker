a00e7997ba1b28356b26fa6c60574651a4ba0571
mattmiller85 <mattmiller85@gmail.com>
2016-09-19 15:59:00 -0400

    mongo temp commit

--- WorkoutTrackerApi/Services/MongoWorkoutDataService.cs
+++ WorkoutTrackerApi/Services/MongoWorkoutDataService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Security.Authentication;
 using MongoDB.Driver;
 using WorkoutTrackerCore;
 
@@ -8,12 +9,30 @@ namespace WorkoutTrackerApi.Services
 {
 	public class MongoWorkoutDataService : IWorkoutDataService
 	{
+		private readonly MongoClient _mongoClient;
+		public MongoWorkoutDataService()
+		{
+			var settings = new MongoClientSettings
+			{
+				Server = new MongoServerAddress(ConfigurationManager.AppSettings["MongoServer"], int.Parse(ConfigurationManager.AppSettings["MongoPort"])),
+				UseSsl = true,
+				SslSettings = new SslSettings { EnabledSslProtocols = SslProtocols.Tls12 }
+			};
+
+			var identity = new MongoInternalIdentity("workouttracker", ConfigurationManager.AppSettings["MongoUser"]);
+			var evidence = new PasswordEvidence(ConfigurationManager.AppSettings["MongoPassword"]);
+
+			settings.Credentials = new List<MongoCredential>
+			{
+				new MongoCredential("SCRAM-SHA-1", identity, evidence)
+			};
+
+			_mongoClient = new MongoClient(settings);
+		}
 		public List<Workout> GetLatest(int count)
 		{
-			var client = new MongoClient(ConfigurationManager.AppSettings["MongoServer"]);
-			var db = client.GetDatabase("workouttracker");
+			var db = _mongoClient.GetDatabase("workouttracker");
 			var collection = db.GetCollection<Workout>("workouts");
-
 			return new List<Workout>();
 		}
 
