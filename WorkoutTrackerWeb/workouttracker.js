d428e24cf4a1b06855253b7d09ab995ea0617c6e
Matt Miller <mattmiller85@gmail.com>
2016-10-28 12:21:38 -0400

    more ui

--- WorkoutTrackerWeb/workouttracker.js
+++ WorkoutTrackerWeb/workouttracker.js
@@ -1,9 +1,29 @@
-﻿var workoutTrackerApp = angular.module('workoutTrackerApp', ['ui.bootstrap']);
+﻿var workoutTrackerApp = angular.module('workoutTrackerApp', ['ui.bootstrap', 'ngRoute']);
+
+workoutTrackerApp.config(function($routeProvider) {
+	$routeProvider
+
+		// route for the home page
+		.when('/', {
+			templateUrl : 'pages/home.html',
+			controller  : 'mainController'
+		})
+
+		// route for the about page
+		.when('/addworkout', {
+			templateUrl : 'pages/addworkout.html',
+			controller  : 'addWorkoutController'
+		})
+});
 
 // create the controller and inject Angular's $scope
-workoutTrackerApp.controller('workoutController', function ($scope) {
+workoutTrackerApp.controller('mainController', function ($scope) {
 
 	$scope.noWorkoutsMessage = 'There are no workouts, click "New Workout" to add one!';
 
 	$scope.workouts = [];
+});
+
+workoutTrackerApp.controller('addWorkoutController', function ($scope) {
+
 });
